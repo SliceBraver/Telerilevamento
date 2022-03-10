1cdf12acf82029534dde35ae5f13d2a96f4510c1
Gianmarco Virzi <44340097+gianmarcovirzi@users.noreply.github.com>
2022-03-10 17:49:36 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,2 +1,16 @@
 # Richiamo la libreria raster
 library(raster)
+
+#set working directory - impostiamo la cartella lab come ambiente di lavoro
+setwd("C:/lab") #Windows
+
+#Importo RasterBrick
+im2011 <- brick("p224r63_2011.grd") 
+im2011 #Visualizziamo i dati importati
+
+#Effettuo il plot (visualizzazione immagine) dei dati importati
+plot(im2011)
+
+#Cambiamo la leggenda dei colori
+c1 <- colorRampPalette(c("black", "grey", "light grey")) (100) #Indico il range di colore, il valore finale indica il numero di passaggi
+plot(im2011, col = cl) #Effettuo nuovamente il plot specificando il range di colori
