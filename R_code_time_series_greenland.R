217a7b49f1c10799afcfc319cb3ef7ef02dfd443
Gianmarco Virzi <44340097+gianmarcovirzi@users.noreply.github.com>
2022-04-02 00:04:56 +0200

    Update R_code_time_series_greenland.R

--- R_code_time_series_greenland.R
+++ R_code_time_series_greenland.R
@@ -1,1 +1,8 @@
 #Time series analysis of Greenland LST Data
+library(raster) 
+
+# setwd("~/lab/greenland") # Linux
+setwd("C:/lab/greenland") # Windows
+# setwd("/Users/name/Desktop/lab/greenland") # Mac
+
+raster <- ("lst_2000.tif")
