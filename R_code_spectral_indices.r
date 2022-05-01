68f6961bea53492efbb3528b4657faa04502ea67
Gianmarco Virzi <44340097+gianmarcovirzi@users.noreply.github.com>
2022-05-01 23:47:15 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -3,6 +3,9 @@ library(raster)
 install.packages("RStoolbox") #Installo il pacchetto RStoolbox
 library(RStoolbox) #Lo carico
 
+install.packages("rasterdiv")
+library(rasterdiv)
+
 setwd("D:/lab/") #Setto la working directory
 
 #Importiamo il primo file - name -> l1992
@@ -39,8 +42,40 @@ plot(dvi2006, col = cl) #Tutto ciò che è rosso è in salute, notare che il ros
 
 #DVI differenza nel tempo
 dvi_dif = dvi1992 - dvi2006
+dvi_dif
 cld <- colorRampPalette(c('blue', 'white', 'red'))(100) #Cambio la palette dei colori
 dev.off() #Chiudo il plot precedente
 plot(dvi_dif, col = cld) #Effettuo il plot della differenza
 
+#================================
+#NDVI - DVI Normalizzato (o Standardizzato)
+#================================
+#DVI standardizzato sulla riflettanza delle due bande
+#NIR - RED / NIR  +RED
+#Range DVI (8bit) = -255 to 255
+#Range NDVI (8bit) = -1 to 1
+#L'NDVI di conseguenza permette il confronto con immagini di diversa risoluzione radiometrica
+ndvi1992 = dvi1992 / (l1992[[1]] + l1992[[2]])
+ndvi1992
+plot(ndvi1992, col = cl)
 
+#Creo il multiframe con plot RGB e NDVI
+par(mfrow=c(2,1))
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plot(ndvi1992, col = cl)
+
+ndvi2006 = dvi2006 / (l2006[[1]] + l2006[[2]])
+par(mfrow=c(2,1))
+plot(ndvi1992, col = cl)
+plot(ndvi2006, col = cl)
+
+#Automatic spectral indices by RStoolbox methods
+si1992 <- spectralIndices(l1992, green=3, red=2, nir = 1)
+plot(si1992, col=cl)
+
+si2006 <- spectralIndices(l2006, green=3, red=2, nir = 1)
+plot(si2006, col=cl)
+
+#Faccio il plot del set di dati rasterdiv
+dev.off()
+plot(copNDVI)
